352a957a5f487b09fcfb47049fc4466f45a1ec19
Dan Murphy <murphydbuffalo@gmail.com>
2017-05-07 16:14:02 -0400

    Add `Cache-Control` headers & unique filenames for manifest files.
    Updating our build and deploy scripts with these changes ensures that browers
    A) know that they can cache images up to a year (the maximum allowed value) for
    `maxage`, and B) will only make requests for our CSS and JS manifest files if
    the contents of our application specific CSS or JS changes (this is possible
    because of our use of manifest file names that include an md5 hash based on the
    file contents of our application CSS and JS code).

--- bin/build.sh
+++ bin/build.sh
@@ -5,26 +5,42 @@ cp ./favicons/* ./build
 mkdir ./build/images
 cp -R ./images ./build
 
+# Concatenate and minify all files in the `/css` and `/javascript` directories.
+# And move the resulting manifest files to the `/build` directory of the project.
+#
+# We make separate calls to `uglify` and `minify` (which is just a wrapper library
+# that can call `uglify` if you want), because we are using the `harmony` branch
+# of `uglify`. This allows us to minify ES6 JavaScript.
+#
+# We also give each manifest file a unique name that includes a hash of the file
+# contents of the corresponding file (either `index.css` and `index.js`).
+# These unique names, combined with a very long `Cache-Control` header `maxage`
+# value allow browsers to avoid making requests for our JS and CSS files for up
+# to one year, so long as the contents of `index.css/js` hasn't changed.
+
+JS_HASH=`md5 -q ./javascript/index.js`
+CSS_HASH=`md5 -q ./javascript/index.js`
+uglifyjs ./javascript/index.js --compress --mangle toplevel --output "./build/index-$JS_HASH.min.js"
+minify --output "./build/index-$CSS_HASH.min.css" ./css/normalize.css ./css/skeleton.css ./css/index.css
+
+# Update `template.html` to use the hash-based filenames for our CSS and JS manifest files.
+JS_MANIFEST=`find ./build -name "index-*.min.js" -exec basename {} \;`
+CSS_MANIFEST=`find ./build -name "index-*.min.css" -exec basename {} \;`
+sed "s/index.min.js/$JS_MANIFEST/" ./template.html > ./template_with_manifests.html
+sed -i "" "s/index.min.css/$CSS_MANIFEST/" ./template_with_manifests.html
+
 # For every file in the `/views` directory prepend the contents of `template.html`
 # and append the closing `</body>` and `</html>` tags, and move the resulting files
-# to the `/buil` directory of the project.
+# to the `/build` directory of the project.
 VIEWS=./views/*.html
 for v in $VIEWS
 do
   FILE=`basename $v`
   echo "Building $FILE"
-  cat ./template.html > ./build/"$FILE"
+  cat ./template_with_manifests.html > ./build/"$FILE"
   cat $v >> ./build/"$FILE"
   echo "\t</body>\n</html>\n" >> ./build/"$FILE"
 done
-
-# Concatenate and minify all files in the `/css` and `/javascript` directories.
-# And move the resulting manifest files to the `/build` directory of the project.
-# We make separate calls to `uglify` and `minify` (which is just a wrapper library
-# that can call `uglify` if you want), because we are using the `harmony` branch
-# of `uglify`. This allows us to minify ES6 JavaScript.
-uglifyjs ./javascript/index.js --compress --mangle toplevel --output ./build/index.min.js
-minify --output ./build/index.min.css ./css/normalize.css ./css/skeleton.css ./css/index.css
 
 if [ -z "$1" ]
 then
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-aws s3 sync ./build s3://www.danmurphy.codes --acl public-read --exclude "*" --include "*.html" --include "*.css" --include "*.js" --content-encoding "gzip"
-aws s3 sync ./build s3://www.danmurphy.codes --acl public-read --exclude "*.html" --exclude "*.css" --exclude "*.js"
+aws s3 sync ./build s3://www.danmurphy.codes --acl public-read --exclude "*" --include "*.html"  --content-encoding "gzip"
+aws s3 sync ./build s3://www.danmurphy.codes --acl public-read --exclude "*" --include "*.css" --include "*.js" --content-encoding "gzip" --cache-control "max-age=31536000"
+aws s3 sync ./build s3://www.danmurphy.codes --acl public-read --exclude "*" --include "images/*" --cache-control "max-age=31536000"
+aws s3 sync ./build s3://www.danmurphy.codes --acl public-read --exclude "*.html" --exclude "*.css" --exclude "*.js" --exclude "images/*"
