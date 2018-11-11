03ae6a6ee4fe93e948b52a4213ff2977fbce2222
Dan Murphy <murphydbuffalo@gmail.com>
2018-11-11 15:20:51 -0700

    Get rid of JS, update build script accordingly.
    Also, use NPM to get dependencies for the build script, rather than
    having them be implicit (eg needing to have the binaries installed and
    in your PATH).

--- bin/build.sh
+++ bin/build.sh
@@ -7,63 +7,19 @@ mkdir -p ./build
 rm -rf ./build/*
 cp -R ./favicons/* ./images ./build
 
-# Concatenate and minify all files in the `/css` and `/javascript` directories.
-# And move the resulting manifest files to the `/build` directory of the project.
+# Concatenate and minify all files in the `/css` directory and move the resulting
+# manifest file to the `/build` directory of the project.
 #
-# We make separate calls to `uglify` and `minify` (which is just a wrapper library
-# that can call `uglify` if you want), because we are using the `harmony` branch
-# of `uglify`. This allows us to minify ES6 JavaScript.
-#
-# We also give each manifest file a unique name that includes the hash of the file
-# contents of the corresponding file (either `index.css` and `index.js`).
-# These unique names, combined with setting the `Cache-Control` header's `maxage`
-# value allow browsers to avoid making requests for our JS and CSS files for up
-# to one year, so long as the contents of `index.css/js` haven't changed.
-JS_HASH=`md5 -q ./javascript/index.js`
-CSS_HASH=`md5 -q ./css/index.css`
-uglifyjs ./javascript/index.js --compress --mangle toplevel --output "./build/index-$JS_HASH.min.js"
-minify --output "./build/index-$CSS_HASH.min.css" ./css/normalize.css ./css/skeleton.css ./css/index.css
+# We also give the manifest file a unique name that includes the hash of the file
+# contents. These unique names, combined with setting the `Cache-Control`
+# header's `maxage` value allow browsers to avoid making requests for our CSS
+# for up to one year, so long as the contents of `index.css` haven't changed.
+CSS_HASH=`./node_modules/md5-file/cli.js ./css/index.css`
+./node_modules/uglifycss/uglifycss --output "./build/index-$CSS_HASH.min.css" ./css/normalize.css ./css/skeleton.css ./css/index.css
 
-# Build full HTML files from `template.html` (which contains the <head> and
-# opening <html> and <body> tags) and the files in `/views`.
-#
-# Replace the CSS and JS file names in `template.html` with the hash-based filenames for our CSS and JS manifest files.
-# And write the resulting HTML to `template_with_manifests.html`.
-JS_FILENAME=`find ./build -name "index-*.min.js" -exec basename {} \;`
 CSS_FILENAME=`find ./build -name "index-*.min.css" -exec basename {} \;`
-sed "s/index.min.js/$JS_FILENAME/" ./template.html > ./template_with_manifests.html
-sed -i "" "s/index.min.css/$CSS_FILENAME/" ./template_with_manifests.html
+sed -i "" "s/index.min.css/$CSS_FILENAME/" ./index.html
 
-# For every file in the `/views` directory prepend the contents of `template_with_manifests.html`
-# and append the closing `</body>` and `</html>` tags, and move the resulting files
-# to the `/build` directory of the project.
-VIEWS=./views/*.html
-for v in $VIEWS
-do
-  FILE=`basename $v`
-  echo "Building $FILE"
-  cat ./template_with_manifests.html > ./build/"$FILE"
-  cat $v >> ./build/"$FILE"
-  cat ./google-analytics.js >> ./build/"$FILE"
-  echo "\t</body>\n</html>\n" >> ./build/"$FILE"
-done
-
-# Passing in any argument to this script will skip the gzip step. Useful for local
-# development with `pythom -m SimpleHTTPServer` which does not let you specify
-# the `Content-Encoding` header, and thus can't server gzipped assets.
-if [ -z "$1" ]
-then
-  # gzip non-binary files
-  echo "Gziping text files..."
-
-  gzip -9 ./build/*.html ./build/*.css ./build/*.js
-
-  # Remove .gz file extensions
-  ZIPPED_FILES=./build/*.gz
-  for z in $ZIPPED_FILES
-  do
-    mv $z `echo $z | sed "s/\.gz//"`
-  done
-fi
+cp index.html ./build/index.html
 
 echo "Shucky ducky."
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,12 +8,11 @@ set -e
 # Set `Content-Encoding` to `gzip` for HTML assets.
 aws s3 sync ./build s3://danmurphy.codes --acl public-read --exclude "*" --include "*.html"  --content-encoding "gzip"
 
-# Set `Content-Encoding` to `gzip` and set `Cache-Control` to `maxage=31536000` for CSS & JS manifest files.
-aws s3 sync ./build s3://danmurphy.codes --acl public-read --exclude "*" --include "*.css" --include "*.js" --content-encoding "gzip" --cache-control "max-age=31536000"
+# Set `Content-Encoding` to `gzip` and set `Cache-Control` to `maxage=31536000` for CSS manifest file.
+aws s3 sync ./build s3://danmurphy.codes --acl public-read --exclude "*" --include "*.css" --content-encoding "gzip" --cache-control "max-age=31536000"
 
 # Set `Cache-Control` to `maxage=31536000` for images.
 aws s3 sync ./build s3://danmurphy.codes --acl public-read --exclude "*" --include "images/*" --cache-control "max-age=31536000"
 
 # Upload remaining files (favicons) as-is.
 aws s3 sync ./build s3://danmurphy.codes --acl public-read --exclude "*.html" --exclude "*.css" --exclude "*.js" --exclude "images/*"
-
